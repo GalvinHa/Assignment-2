b33b0e04175f6431939f52616656bf26be1447f8
Galvin Ha <924422782@sfsu.edu>
2025-09-29 12:00:21 -0700

    first commit

---
+++ EventTicket340.cpp
@@ -0,0 +1,20 @@
+// TO DO: Implementation of EventTicket340 functions
+#include <iostream>
+#include <string>
+#include "EventTicket340.h"
+
+// TO DO: implement constructor
+
+// TO DO: implement destructor
+
+void EventTicket340::createOrganizer(const std::string& username, const std::string& email, const std::string& password, const std::string& bio, const std::string& profilePicture){
+	// TO DO: implement function
+	LinkedBag<Oragnizer> LB;
+	LB.add(new Organizer(username, email, password, bio, profilePicture));
+	
+}
+
+Organizer EventTicket340::getOrganizer() const{
+	// TO DO: implement function
+	return Organizer;
+}
